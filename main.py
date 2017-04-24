e8faf611fcbe53fe8a75d543904ab58c24004a1d
hongyi jiang <jhy@hongyis-MacBook-Pro.local>
2017-04-24 11:49:05 -0400

    add svm model

---
+++ main.py
@@ -0,0 +1,11 @@
+from model import VGG16
+from utils import Data
+import h5py
+
+
+root_path = '../../ece6254_data'
+vgg16_weights_path = '../../ece6254_data/vgg16_weights.h5'
+our_weights_path = '../../ece6254_data/weights.h5'
+data = Data(root_path, load_size=100)
+model = VGG16(our_weights_path, vgg16_weights_path)
+model.fit(data, batch_size=32, nb_epoch=1)
